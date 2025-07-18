96deac9f667466cebacf22fcd12de4a7f6df27dc
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2025-07-18 12:22:43 +0100

    creation de la classe client

--- src/main/java/org/gestion/encheres/model/Client.java
+++ src/main/java/org/gestion/encheres/model/Client.java
@@ -1,8 +1,6 @@
 package org.gestion.encheres.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Client {
@@ -14,8 +12,19 @@ public class Client {
     private String username;
     private String password;
     private String telephone;
+    @Enumerated(EnumType.STRING)
+    private Type type;
 
-    private String type;
+
+    public Client(){
+
+    }
+    public Client(String username, String password, String telephone, Type type){
+        this.username = username;
+        this.password = password;
+        this.telephone = telephone;
+        this.type = type;
+    }
 
 
 
@@ -25,5 +34,37 @@ public class Client {
 
     public Long getId() {
         return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
     }
 }
